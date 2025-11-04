e71e1a4cd00890d7e451e7af5d834c69eec29a96
Abdul Rahim <abdul.rahim@paysyslabs.com>
2025-11-04 15:25:07 +0500

    fix: updated version to 1.0.7

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,6 @@ export * from './interfaces/core.interfaces';
 export * from './interfaces/schema.interfaces';
 export * from './interfaces/json-schema.interfaces';
 export * from './interfaces/multi-field-mapping.interfaces';
-export * from './interfaces/iMappingConfiguration';
-export * from './interfaces/iMappingResult';
 export * from './interfaces/Endpoint';
 export * from './interfaces/enrichment.interface';
 
